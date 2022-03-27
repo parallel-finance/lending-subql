7b656f55e96bd7acbc09749a2ded3f2b5f707f27
Bruce <BruceChar@163.com>
2022-03-28 00:25:25 +0800

    add new schema & events file

--- src/mappings/events.ts
+++ src/mappings/events.ts
@@ -1,3 +1,6 @@
+import { GenericEventData } from "@polkadot/types"
+import { SubstrateEvent } from "@subql/types"
+import { LendingAction } from "../types"
 
 interface Transfer {
     sender: string,
@@ -5,13 +8,13 @@ interface Transfer {
     amount: string
 }
 
-interface Deposited extends Transfer {}
+interface Deposited extends Transfer { }
 
-interface Redeemed extends Transfer {}
+interface Redeemed extends Transfer { }
 
-interface Brrowed extends Transfer {}
+interface Brrowed extends Transfer { }
 
-interface RepaidBorrow extends Transfer {}
+interface RepaidBorrow extends Transfer { }
 
 interface LiquidatedBorrow {
     liquidator: string,
@@ -29,11 +32,11 @@ interface AdminEvent {
     assetId: number
 }
 
-interface NewMarket extends AdminEvent {}
+interface NewMarket extends AdminEvent { }
 
-interface ActivatedMarket extends AdminEvent {}
+interface ActivatedMarket extends AdminEvent { }
 
-interface UpdateMarket extends AdminEvent {}
+interface UpdateMarket extends AdminEvent { }
 
 interface ReservesReduced extends AdminEvent {
     reduceAmount: string,
@@ -43,4 +46,65 @@ interface ReservesReduced extends AdminEvent {
 interface ReservesAdded extends AdminEvent {
     addAmount: string,
     totalReserves: string
+}
+
+export enum MmEvent {
+    Transfer,
+    Liquidate,
+    Reserve,
+    Market,
+    Unknow
+}
+
+export function getEvtType(method: string): MmEvent {
+    switch (method) {
+        case 'Deposited':
+        case 'Redeemed':
+        case 'Borrowed':
+        case 'RepaidBorrow':
+            return MmEvent.Transfer
+        case 'LiquidatedBorrow':
+            return MmEvent.Liquidate
+        case 'NewMarket':
+        case 'ActivateMarket':
+        case 'UpdateMarket':
+            return MmEvent.Market
+        case 'ReservesAdded':
+        case 'ReservesReduced':
+            return MmEvent.Reserve
+        default:
+            logger.warn(`unknow method to handle: ${method}`)
+            return MmEvent.Unknow
+
+    }
+}
+
+export async function eventHandler(event: SubstrateEvent) {
+    try {
+        let { event: { data, method } } = event
+        const ext = event.extrinsic
+        const hash = ext.extrinsic.hash
+        const blockHeight = ext.block.block.header.number.toNumber()
+        const timestamp = ext.block.timestamp
+        const evt = getEvtType(method)
+        switch (evt) {
+            case MmEvent.Transfer:
+                let [sender, assetId, amount] = data
+                LendingAction.create({
+                    id: '',
+                    address: sender.toString()
+                })
+                break
+            case MmEvent.Liquidate:
+                break
+            case MmEvent.Market:
+                break
+            case MmEvent.Reserve:
+                break
+            case MmEvent.Unknow:
+                logger.error(`unknow event to handler`)
+        }
+    } catch (e: any) {
+        logger.error(`handle event error: ${e.message}`)
+    }
 }
